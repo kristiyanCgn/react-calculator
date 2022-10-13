4d1722d0cbdfbfe2d4e12855e88515003ffbcb80
kristiyanCgn <101466719+kristiyanCgn@users.noreply.github.com>
2022-10-13 15:39:11 +0200

    functionality for different operations

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -26,9 +26,56 @@ function reducer (state, { type, payload }){
                 ...state,
                 currentOperand: `${state.currentOperand || ""}${payload.digit}`
             }
+            case ACTIONS.CHOOSE_OPERATION:
+                if(state.currentOperand == null && state.previousOperand == null) {
+                    return state
+                }
+
+                if (state.previousOperand == null) {
+                    return {
+                        ...state,
+                        operation: payload.operation,
+                        previousOperand: state.currentOperand,
+                        currentOperand: null
+                    }
+                }
+
+                return {
+                    ...state,
+                    previousOperand: evaluate(state),
+                    operation: payload.operation,
+                    currentOperand: null
+                }
+
             case ACTIONS.CLEAR:
                 return {}
     }
+}
+
+function evaluate ({ currentOperand, previousOperand, operation }) {
+    const prev = parseFloat(previousOperand)
+    const current = parseFloat(currentOperand)
+
+    if(isNaN(prev) || isNaN(current)) return ""
+
+    let computation = ""
+
+    switch (operation) {
+        case "+":
+            computation = prev + current
+            break
+        case "-":
+            computation = prev - current
+            break
+        case "*":
+            computation = prev * current
+            break
+        case "÷":
+            computation = prev / current
+            break
+    }
+
+    return computation.toString()
 }
 
 function App() {
@@ -47,19 +94,19 @@ function App() {
                 AC
             </button>
             <button>DEL</button>
-            <OperationButton digit="÷" dispatch={dispatch} />
+            <OperationButton operation="÷" dispatch={dispatch} />
             <DigitButton digit="1" dispatch={dispatch} />
             <DigitButton digit="2" dispatch={dispatch} />
             <DigitButton digit="3" dispatch={dispatch} />
-            <OperationButton digit="*" dispatch={dispatch} />
+            <OperationButton operation="*" dispatch={dispatch} />
             <DigitButton digit="4" dispatch={dispatch} />
             <DigitButton digit="5" dispatch={dispatch} />
             <DigitButton digit="6" dispatch={dispatch} />
-            <OperationButton digit="+" dispatch={dispatch} />
+            <OperationButton operation="+" dispatch={dispatch} />
             <DigitButton digit="7" dispatch={dispatch} />
             <DigitButton digit="8" dispatch={dispatch} />
             <DigitButton digit="9" dispatch={dispatch} />
-            <OperationButton digit="-" dispatch={dispatch} />
+            <OperationButton operation="-" dispatch={dispatch} />
             <DigitButton digit="." dispatch={dispatch} />
             <DigitButton digit="0" dispatch={dispatch} />
             <button className="span-two">=</button>
